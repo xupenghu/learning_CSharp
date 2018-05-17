9838003c406c8062a4cd23836002b1786306b3c0
xupenghu <xupenghu@outlook.com>
2018-05-17 23:23:29 +0800

    add lesson1

---
+++ lesson1_FistProgram/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class TestClass
+    {
+        const double Pi = 3.1415;
+        readonly int i;
+        TestClass()
+        {
+            i = 100;
+        }
+
+    }
+    class Program
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.Write("hello world!\n");
+            Console.ReadLine();
+
+        }
+    }
+}
