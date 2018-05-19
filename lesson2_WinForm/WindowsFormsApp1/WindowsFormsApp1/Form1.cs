bb2b7056c459c8edb8e7980e9b7ff232fc62b7b0
xupenghu <xupenghu@outlook.com>
2018-05-19 12:23:18 +0800

    add lesson2

---
+++ lesson2_WinForm/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a = double.Parse(textBox1.Text);
+            double b = Math.Sqrt(a);
+            label1.Text = a + "的平方根是" + b.ToString();
+        }
+
+    }
+}
